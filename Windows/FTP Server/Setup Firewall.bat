6038fd09a8275e870f886594360fdd9c38826e50
LucianDev <5789235+LucianDev@users.noreply.github.com>
2025-07-07 13:47:52 -0500

    Start of FTP Tutorial

---
+++ Windows/FTP Server/Setup Firewall.bat
@@ -0,0 +1,12 @@
+@echo off
+echo Opening FTP ports in Windows Firewall...
+
+:: Open FTP control port (21)
+netsh advfirewall firewall add rule name="FTP Port 21" dir=in action=allow protocol=TCP localport=21
+
+:: Open FTP passive port range (50000–50100)
+netsh advfirewall firewall add rule name="FTP Passive Ports" dir=in action=allow protocol=TCP localport=50000-50100
+
+echo.
+echo ✅ FTP ports opened successfully!
+pause
